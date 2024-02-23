93d111c258077cd9c7c647b3187c480951a3e2a1
Dawid Płocki <dawid.plocki@gmail.com>
2024-02-23 15:25:51 +0100

    Samplary code

--- PsychicWaddle/Program.cs
+++ PsychicWaddle/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AutoMapper;
+
+namespace AutoMapperDemo
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+    }
+
+    public class EmployeeDTO
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+    }
+
+    public class MapperConfig
+    {
+        public static Mapper InitializeAutomapper()
+        {
+            //Provide all the Mapping Configuration
+            var config = new MapperConfiguration(cfg =>
+            {
+                //Configuring Employee and EmployeeDTO
+                cfg.CreateMap<Employee, EmployeeDTO>();
+                //Any Other Mapping Configuration ....
+            });
+            //Create an Instance of Mapper and return that Instance
+            var mapper = new Mapper(config);
+            return mapper;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create and Populate Employee Object i.e. Source Object
+            Employee emp = new Employee
+            {
+                Name = "James",
+                Salary = 20000,
+                Address = "London",
+                Department = "IT"
+            };
+
+            //Initializing AutoMapper
+            var mapper = MapperConfig.InitializeAutomapper();
+
+            //Way1: Specify the Destination Type and to The Map Method pass the Source Object
+            //Now, empDTO1 object will having the same values as emp object
+            var empDTO1 = mapper.Map<EmployeeDTO>(emp);
+
+            //Way2: Specify the both Source and Destination Type 
+            //and to The Map Method pass the Source Object
+            //Now, empDTO2 object will having the same values as emp object
+            var empDTO2 = mapper.Map<Employee, EmployeeDTO>(emp);
+
+            Console.WriteLine("Name: " + empDTO1.Name + ", Salary: " + empDTO1.Salary + ", Address: " + empDTO1.Address + ", Department: " + empDTO1.Department);
+            Console.ReadLine();
+        }
+    }
+}

e4ac30463190c7086d725b66e5ea0639b8e818d8
Dawid Płocki <dawid.plocki@gmail.com>
2024-02-24 20:48:31 +0100

    Refactor

--- PsychicWaddle/Program.cs
+++ PsychicWaddle/Program.cs
@@ -6,12 +6,11 @@ namespace AutoMapperDemo
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
     }
 
     public class Employee
     {
-        public IEnumerable<Role> SomeCollection { get; set; }
+        public IEnumerable<Role> Roles { get; set; }
         public string Name { get; set; }
         public int Salary { get; set; }
         public string Address { get; set; }
@@ -20,7 +19,7 @@ namespace AutoMapperDemo
 
     public class EmployeeDTO
     {
-        public IEnumerable<Role> SomeCollection { get; set; }
+        public IEnumerable<Role> Roles { get; set; }
         public string Name { get; set; }
         public int Salary { get; set; }
         public string Address { get; set; }
@@ -31,14 +30,11 @@ namespace AutoMapperDemo
     {
         public static Mapper InitializeAutomapper()
         {
-            //Provide all the Mapping Configuration
             var config = new MapperConfiguration(cfg =>
             {
-                //Configuring Employee and EmployeeDTO
                 cfg.CreateMap<Employee, EmployeeDTO>();
-                //Any Other Mapping Configuration ....
             });
-            //Create an Instance of Mapper and return that Instance
+
             var mapper = new Mapper(config);
             return mapper;
         }
@@ -48,36 +44,37 @@ namespace AutoMapperDemo
     {
         static void Main(string[] args)
         {
-            //Create and Populate Employee Object i.e. Source Object
+            var mapper = MapperConfig.InitializeAutomapper();
+
             Employee emp = new Employee
             {
                 Name = "James",
                 Salary = 20000,
                 Address = "London",
                 Department = "IT",
-                SomeCollection = new List<Role>()
+                Roles = new List<Role>()
                 {
                     new Role() { Id = 1, Name = "A" },
                     new Role() { Id = 2, Name = "B" },
                     new Role() { Id = 3, Name = "C" }
                 }
             };
-
-            //Initializing AutoMapper
-            var mapper = MapperConfig.InitializeAutomapper();
-
-            //Way1: Specify the Destination Type and to The Map Method pass the Source Object
-            //Now, empDTO1 object will having the same values as emp object
+            
             var empDTO1 = mapper.Map<EmployeeDTO>(emp);
-
-            //Way2: Specify the both Source and Destination Type 
-            //and to The Map Method pass the Source Object
-            //Now, empDTO2 object will having the same values as emp object
             var empDTO2 = mapper.Map<Employee, EmployeeDTO>(emp);
 
+            Console.WriteLine("empDTO1");
             Console.WriteLine("Name: " + empDTO1.Name + ", Salary: " + empDTO1.Salary + ", Address: " + empDTO1.Address + ", Department: " + empDTO1.Department + ", SomeCollection:");
 
-            foreach(var entry in empDTO1.SomeCollection)
+            foreach(var entry in empDTO1.Roles)
+            {
+                Console.WriteLine(" >> " + entry.Id + " " + entry.Name);
+            }
+
+            Console.WriteLine("empDTO2");
+            Console.WriteLine("Name: " + empDTO2.Name + ", Salary: " + empDTO2.Salary + ", Address: " + empDTO2.Address + ", Department: " + empDTO2.Department + ", SomeCollection:");
+
+            foreach (var entry in empDTO2.Roles)
             {
                 Console.WriteLine(" >> " + entry.Id + " " + entry.Name);
             }
